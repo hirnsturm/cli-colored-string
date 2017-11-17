82bc6e797989314d0508db5eb16448bac82b8779
Steve Lenz <sle@xima.de>
2017-11-17 15:15:27 +0100

    [Bugfix] No colored string returned

--- src/ColoredString.php
+++ src/ColoredString.php
@@ -11,44 +11,40 @@ namespace Sle\CliColoredString;
 class ColoredString
 {
 
-    private static $foregroundColors = [];
-    private static $backgroundColors = [];
+    /**
+     * @var array
+     */
+    private static $foregroundColors = [
+        'dark_gray'    => '1;30',
+        'blue'         => '0;34',
+        'light_blue'   => '1;34',
+        'green'        => '0;32',
+        'light_green'  => '1;32',
+        'cyan'         => '0;36',
+        'light_cyan'   => '1;36',
+        'red'          => '0;31',
+        'light_red'    => '1;31',
+        'purple'       => '0;35',
+        'light_purple' => '1;35',
+        'brown'        => '0;33',
+        'yellow'       => '1;33',
+        'light_gray'   => '0;37',
+        'white'        => '1;37',
+    ];
 
     /**
-     * ColoredString constructor.
+     * @var array
      */
-    public function __construct()
-    {
-        // Set up shell colors
-        self::$foregroundColors = [
-            'dark_gray'    => '1;30',
-            'blue'         => '0;34',
-            'light_blue'   => '1;34',
-            'green'        => '0;32',
-            'light_green'  => '1;32',
-            'cyan'         => '0;36',
-            'light_cyan'   => '1;36',
-            'red'          => '0;31',
-            'light_red'    => '1;31',
-            'purple'       => '0;35',
-            'light_purple' => '1;35',
-            'brown'        => '0;33',
-            'yellow'       => '1;33',
-            'light_gray'   => '0;37',
-            'white'        => '1;37',
-        ];
-
-        self::$backgroundColors = [
-            'black'      => '40',
-            'red'        => '41',
-            'green'      => '42',
-            'yellow'     => '43',
-            'blue'       => '44',
-            'magenta'    => '45',
-            'cyan'       => '46',
-            'light_gray' => '47',
-        ];
-    }
+    private static $backgroundColors = [
+        'black'      => '40',
+        'red'        => '41',
+        'green'      => '42',
+        'yellow'     => '43',
+        'blue'       => '44',
+        'magenta'    => '45',
+        'cyan'       => '46',
+        'light_gray' => '47',
+    ];
 
     /**
      * Returns the colored string
@@ -63,12 +59,12 @@ class ColoredString
         $colored_string = "";
 
         // Check if given foreground color found
-        if (isset(self::$foregroundColors[$foregroundColor])) {
-            $colored_string .= "\033[" . self::$foregroundColors[$foregroundColor] . "m";
+        if (array_key_exists($foregroundColor, static::$foregroundColors)) {
+            $colored_string .= "\033[" . static::$foregroundColors[$foregroundColor] . "m";
         }
         // Check if given background color found
         if (isset(self::$backgroundColors[$backgroundColor])) {
-            $colored_string .= "\033[" . self::$backgroundColors[$backgroundColor] . "m";
+            $colored_string .= "\033[" . static::$backgroundColors[$backgroundColor] . "m";
         }
 
         // Add string and end coloring
@@ -82,7 +78,7 @@ class ColoredString
      */
     public static function getForegroundColors()
     {
-        return array_keys(self::$foregroundColors);
+        return array_keys(static::$foregroundColors);
     }
 
     /**
@@ -90,7 +86,7 @@ class ColoredString
      */
     public static function getBackgroundColors()
     {
-        return array_keys(self::$backgroundColors);
+        return array_keys(static::$backgroundColors);
     }
 
 }
